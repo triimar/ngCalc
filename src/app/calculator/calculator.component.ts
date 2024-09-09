13fab9772ef16db4694bacc9129b1832479ecb67
Marts <triin.marts@ferdinand-steinbeis-institut.de>
2024-09-09 16:32:12 +0200

    Frontend: Layout set

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import { DisplayComponent } from '../display/display.component';
-import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [DisplayComponent, ButtonComponent],
+  imports: [CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
+
 export class CalculatorComponent {
-  show: string = '';
-  onButtonClick(value: string) {
+  display: string = '';
+  digitButtons = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+  operatorButtons = ['+', '-', '*', '/'];
+  controlButtons = ['C', '='];
+  
+
+  onDigitClick(value: string) {
     console.log('Button: ', value);
-    this.show = value;
+    this.display += value;
   }
 
 }
