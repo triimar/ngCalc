aac0413e7064aca509633d9662b69746381fab97
Marts <triin.marts@ferdinand-steinbeis-institut.de>
2024-09-13 21:49:11 +0200

    Minor bugs left

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 })
 
 export class CalculatorComponent {
-  display: string = '';
+  display: string = '0';
   previousResult: number = 0;
   operator: string = '';
 
@@ -18,54 +18,84 @@ export class CalculatorComponent {
   digitButtons = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '+/-', '0', '.'];
   operatorButtons = ['+', '-', '*', '/'];
 
-  controlButtons = ['CE', 'C', '='];
+  controlButtons = ['C', 'CE', '='];
   
 
   onDigitClick(value: string) {
-    this.display += value;
+
+    if (value === '.' && this.display.indexOf('.') !== -1) {
+      return;
+    }
+    if (value === '+/-') {
+      if (this.display.charAt(0) === '-') {
+        this.display = this.display.substring(1);
+      } else {
+        this.display = '-' + this.display;
+      }
+      return;
+    }
+    if (this.display === '0' && value !== '.') {
+      this.display = value;
+    } else {
+      this.display += value;
+    }
   }
 
-  onClick(value: string) {
+  getDisplay(): string {
+    return this.display === '' ? this.previousResult.toString() : this.display;
+  }
+
+  onControlClick(value: string) {
+    this.logAll();
     switch (value) {
-      case 'CE':
-        this.display = '';
+      case 'C':
+        this.display = '0';
         this.previousResult = 0;
         break;
-      case 'C':
-        this.display = '';
+      case 'CE':
+        this.display = '0';
         break;
       case '=':
-        console.log(value);
+        this.calculate();
         this.operator = '';
+        this.display = '';
+        // this.previousResult = 0;
         break;
-      case '+':
-        console.log(value);
-        break;
-      case '-':
-        console.log(value);
-        break;
-      case '*':
-        console.log(value);
-        break;
-      case '/':
-        console.log(value);
-        break;
-      default:
-        this.onDigitClick(value);
-      
     }
   }
-  calculate() {
-    if (this.operator === '+') {
-      this.previousResult += parseFloat(this.display);
-    } else if (this.operator === '-') {
-      this.previousResult -= parseFloat(this.display);
-    } else if (this.operator === '*') {
-      this.previousResult *= parseFloat(this.display);
-    } else if (this.operator === '/') {
-      this.previousResult /= parseFloat(this.display);
+
+  onOperatorClick(value: string) {
+    this.logAll();
+    this.calculate();
+    if (this.operator === '' && this.display !== '') {
+      this.previousResult = parseFloat(this.display);
     }
-    this.display = this.previousResult.toString();
+    this.operator = value;
+    this.display = '';
+    this.logAll(); 
+  }
+
+  roundTo(value: number, decimals: number): number {
+    return Number(Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals));
+  }
+
+  calculate() {
+    const currentNumber = parseFloat(this.display);
+    const precision = 10;  // Adjust precision as needed
+
+    if (this.operator === '+') {
+      this.previousResult = this.roundTo(this.previousResult + currentNumber, precision);
+    } else if (this.operator === '-') {
+      this.previousResult = this.roundTo(this.previousResult - currentNumber, precision);
+    } else if (this.operator === '*') {
+      this.previousResult = this.roundTo(this.previousResult * currentNumber, precision);
+    } else if (this.operator === '/') {
+      this.previousResult = this.roundTo(this.previousResult / currentNumber, precision);
+    }
+  }
+
+  logAll() {
+    console.log("Prev:", this.previousResult, "Operator: ", this.operator, "Display: ", this.display);
   }
 }
 
